0812a4b17a202c09ef3f4933e294e03451176261
ibrahimadiagneseck <ibrahimadiagneseck@esp.sn>
2025-09-08 12:47:23 +0000

    nouveau

---
+++ gestion-corrier-back-end/gestion-corrier-back-end/src/main/java/sn/encvr/gestion_corrier_back_end/GestionCorrierBackEndApplication.java
@@ -0,0 +1,13 @@
+package sn.encvr.gestion_corrier_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCorrierBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCorrierBackEndApplication.class, args);
+	}
+
+}
---
+++ gestion-corrier-back-end/gestion-corrier-back-end/src/test/java/sn/encvr/gestion_corrier_back_end/GestionCorrierBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.encvr.gestion_corrier_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCorrierBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
